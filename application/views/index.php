c9f4abd47a5e7578850238c29beec36fe4e81448
Krisna Putra <krisna@icubeonline.com>
2014-02-13 22:08:04 +0700

    restyling homepage

--- application/views/index.php
+++ application/views/index.php
@@ -31,8 +31,9 @@
 			<div class="middle">
 				<div class="welcome">
 					<h3>Welcome <span>Message</span></h3>
-					<p class="caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p class="caption">Janganlah kiranya kasih dan setia meninggalkan engkau! Kalungkanlah itu pada lehermu, tuliskanlah itu pada loh hatimu, maka engkau akan mendapat kasih dan penghargaan dalam pandangan Allah serta manusia.</p>
+					<!--<p>(Amsal 3:3-4)</p>-->
+					<p>Setiap orang yang memiliki hubungan secara pribadi dengan Yesus Kristus dan percaya bahwa Dia adalah Anak Allah, tidak peduli bagaimana masa lalu anda, darimana anda berasal, apakah anda masih single atau jomblo, sudah punya pacar, atau bahkan sudah menikah, karena Jodoh Kristen tidak hanya tentang jodoh atau pasangan hidup saja, melainkan juga tentang persahabatan dan persekutuan Kristen. </p>
 					<a href="#">Selengkapnya</a>
 				</div>
 				<div class="how-to-join">
@@ -40,19 +41,19 @@
 						<li>
 							<div class="step one">
 								<span>Daftar Sekarang</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+								<p>Bergabunglah sekarang dengan mengisikan data anda pada form registrasi, Pendaftaran gratis!</p>
 							</div>
 						</li>
 						<li>
 							<div class="step two">
 								<span>Buat Profile</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+								<p>Buat profil anda sesuai dengan kondisi anda sebenarnya agar anggota lain dapat mengetahui anda dari profil yang anda buat</p>
 							</div>
 						</li>
 						<li>
 							<div class="step three">
 								<span>Temukan Cintamu</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+								<p>Mulailah menjalin komunikasi dengan anggota lain, dan temukan kecocokan dalam menjalin persahabatan</p>
 							</div>
 						</li>
 					</ul>
@@ -62,13 +63,14 @@
 			<div class="bottom">
 				<div class="why-to-join">
 					<h3>Mengapa Perlu <span>Bergabung?</span></h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<a href="#">Selengkapnya</a>
+					<p>Melalui komunitas ini, anda bisa bertemu dan mengenal dengan orang-orang dalam satu kasih Tuhan Yesus Kristus. Dan juga tidak menutup kemungkinan anda akan menemukan pasangan hidup yang seiman. </p>
+					<br/>
                                         <a href="<?php echo base_url().'register' ?>" class="button">Bergabung Sekarang</a>
 				</div>
 				<div class="follow-us">
 					<h3>Follow <span>Us</span></h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>Jangan lewatkan juga berita-berita terbaru tentang SAHABATKRISTIANI melalui media social berikut</p><br/>
+					<p><a href="#"><img src="<?php echo base_url('images/facebook.png')?>"/></a>&nbsp;&nbsp;<a href="#"><img src="<?php echo base_url('images/twitter.png')?>"/></a></p>
 				</div>
 				<div class="clear"></div>
 				<div class="send-message">
