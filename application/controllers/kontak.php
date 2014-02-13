bca72b179e0883a5c1b6e77c9c847db054827c87
Krisna Putra <krisna@icubeonline.com>
2014-02-13 15:16:49 +0700

    styling

--- application/controllers/kontak.php
+++ application/controllers/kontak.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Pencarian extends CI_Controller {
+class Kontak extends CI_Controller {
 
 	public function index()
 	{
--- application/views/tips.php
+++ application/views/tips.php
@@ -16,7 +16,20 @@
 	<?php $this->load->view('common/nav') ?>
 	<div id="content">
 	<!--bagian ini untuk kontenya--> 
-        
+        <h2 class="page-title">Tips Email &amp; Chatting</h2>
+        <div class="text">
+	        <p>Anda tidak mendapatkan jawaban dari surat-surat anda? Anda jadi putus asa? Tenang. Jangan dipikirkan. Jangan terlalu cepat berkecil hati atau kecewa. </p>
+	        <p>Pastikan bahwa pengharapan anda untuk menemukan pasangan hidup tidak terlalu cepat. Orang lain akan memperhatikan hal itu dan mungkin menjadi "takut". </p>
+	        <p>Jangan fokus pada "tujuan", tapi tenang dan nikmati "alatnya", yaitu semua hal yang SAHABATKRISTIANI tawarkan kepada anda, seperti kegembiraan saat menerima pesan baru, persekutuan Kristen, teman-teman baru yang anda dapat, hal-hal baru yang anda pelajari tentang budaya lain dan kobaran semangat dari Kristus yang anda terima melalui kesaksian dari anggota SAHABATKRISTIANI yang lain. </p>
+	        <p>Jangan duduk dan menunggu hingga orang lain menghampiri anda. Tapi beranikan diri untuk mengambil langkah pertama dan hampiri single Kristen yang lain di SAHABATKRISTIANI. Tidak ada ruginya jika anda hanya mengirimkan pesan yang ramah, kan? </p>
+	        <p>Jangan terlalu mendesak pada email-email awal atau sesi chat awal anda dengan seseorang. Bersikaplah santai dan wajar.</p>
+	        <p>Periksa ejaan anda sebelum mengirimkan pesan. Hal itu menunjukkan bahwa anda rapi dan teliti dan anda "melakukan suatu usaha" dalam menulis surat tersebut. Di sisi lain, kekeliruan fatal dalam ejaan membuat pesan anda kurang menyenangkan untuk dibaca. </p>
+	        <p>Ulang tahun adalah kesempatan yang bagus untuk berkenalan dengan seseorang. Lihatlah siapa yang berulang tahun dan berikan sebaris ucapan "Selamat Ulang Tahun". Anda juga bisa memberikan sambutan kepada anggota baru. </p>
+	        <p>Personalisasikan surat-surat anda. Pastikan setiap email yang anda kirimkan berbeda dan tergantung dari si penerima. Sebagai contoh, anda bisa memfokuskan suatu hal dari profil seseorang. Juga, buatlah spesifik: jangan hanya berkata bahwa anda suka profilnya tapi katakan apa yang anda suka mengenai profilnya. Hal ini menunjukkan bahwa anda benar-benar perhatian dan menginvestasikan waktu anda untuknya.</p>
+	        <p>Ikut sertakan Tuhan dalam pencarian anda untuk bertemu dengan orang-orang baru dan menemukan pasangan hidup anda. Bacalah Alkitab atau renungan harian secara teratur. Seringlah berdoa sehingga Roh Kudus memiliki kesempatan untuk menyatakan rencana Tuhan yang sempurna untuk hidup anda dan hubungan-hubungan anda selanjutnya.</p>
+	        <p>Ingatlah bahwa anda adalah seorang Kristen. Kita adalah duta Kristus. Hal itu merupakan tanggung jawab yang penting ? terhadap satu sama lain dan untuk diri kita sendiri. Oleh karena itu berhati-hatilah dengan sikap dan tutur kata anda apabila anda berkencan dan berhubungan dengan single-single yang lain di SAHABATKRISTIANI. Berlakulah ramah dan sopan, perlakukan satu sama lain dengan hormat. Di dalam Tuhan, kita semua adalah saudara.</p>
+	        <p>Tidak ada ruang di SAHABATKRISTIANI untuk membuntuti, menipu, menggosip, membicarakan mengenai seks secara tidak sopan, spam, mengganggu (secara seksual) dan pelanggaran lain terhadap ketentuan dari kami. Sebenarnya, seharusnya tidak ada ruang sama sekali untuk hal-hal seperti yang tersebut di atas di kehidupan orang-orang Kristen. </p>
+        </div>
         
         <!--end content-->
         </div>
